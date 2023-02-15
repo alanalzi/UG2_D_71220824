70f52286c71dd1f119713344e3ec68cda2e9c9bb
alanalzi <117884402+alanalzi@users.noreply.github.com>
2023-02-15 19:29:40 +0700

    Add files via upload

--- 3_D_71220824.py
+++ 3_D_71220824.py
@@ -11,7 +11,25 @@ while c > dx:
         print('Diskon 25%')
         disk = har * (25/100)
         hartot = har - disk
-        print('Diskon',disk,'harga setelah diskon',hartot,'harga sebelum diskon',har)
-        th = th + hartot
-        td = th + disk
-        
+        print('Diskon',disk,'harga saat diskon',hartot,'harga belum diskon',har)
+        th +=  hartot
+        td +=  disk
+        dx += 1
+        dy += 1
+    elif har > 10000000:
+        print('Diskon 10%')
+        disk = har * (10/100)
+        hartot = har - disk
+        print('Diskon',disk,'harga saat diskon',hartot,'harga belum diskon',har)
+        th +=  hartot
+        td +=  disk
+        dx += 1
+        dy += 1
+    else:
+        hartot = har
+        disk = 0
+        print('Diskon',disk,'harga saat diskon',hartot,'harga belum diskon',har)
+        dx += 1
+        dy += 1
+print('harga akhir', )
+
